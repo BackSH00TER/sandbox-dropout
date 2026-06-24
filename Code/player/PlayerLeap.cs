07b4f50c1f5b50caa3da4a07b9ee8cfaa062db3e
denverjohnston <277109418+denverjohnston@users.noreply.github.com>
2026-06-23 22:10:53 -0700

    Updated leap logic

--- Code/player/PlayerLeap.cs
+++ Code/player/PlayerLeap.cs
@@ -1,27 +1,37 @@
+using System.Numerics;
 using Sandbox;
 
 public sealed class PlayerLeap : Component, PlayerController.IEvents
 {
 	[Property] PlayerController TargetController { get; set; }
+	[Property] GameObject TargetBody { get; set; }
 	[Property] SkinnedModelRenderer TargetRenderer { get; set; }
+	[Property] float LeapCooldown { get; set; } = 3f;
 
 	bool isLeaping = false;
-	private TimeUntil leapCooldown = -1f;
+	private TimeUntil leapCooldownTime = -1f;
+
 
 	protected override void OnFixedUpdate()
 	{
+		if ( IsProxy ) return;
+
 		if ( TargetController.UseInputControls == false )
 			return;
 
-		if ( Input.Pressed( "attack1" ) && isLeaping == false && leapCooldown )
+		if ( !TargetController.IsOnGround )
+			return;
+
+		if ( Input.Pressed( "attack1" ) && isLeaping == false && leapCooldownTime )
 			BeginLeap();
 	}
 
-	private void BeginLeap()
+	[Rpc.Broadcast]
+	public void BeginLeap()
 	{
 		// Set state
 		isLeaping = true;
-		leapCooldown = 3f;
+		leapCooldownTime = LeapCooldown;
 
 		// Update properties
 		TargetController.UseInputControls = false;
@@ -30,12 +40,16 @@ public sealed class PlayerLeap : Component, PlayerController.IEvents
 		// Get movement variables
 		var rb = TargetController.GetComponent<Rigidbody>();
 		Vector3 upVelocity = Vector3.Up * 400f;
-		Vector3 forwardVelocity = TargetController.EyeAngles.Forward * 400f;
+		Vector3 flatEyeAngle = new Vector3( TargetController.EyeAngles.Forward.x, TargetController.EyeAngles.Forward.y, 0 );
+		Vector3 forwardVelocity = flatEyeAngle.Normal * 400f;
 		Vector3 leapVelocity = upVelocity + forwardVelocity;
 
 		// Apply movement
 		TargetController.Jump( upVelocity );
 		rb.Velocity = leapVelocity;
+
+		Rotation newRotation = Rotation.LookAt( forwardVelocity );
+		TargetBody.WorldRotation = newRotation;
 	}
 
 	void PlayerController.IEvents.OnLanded( float distance, Vector3 impactVelocity )
