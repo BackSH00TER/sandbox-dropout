0730644e1ece4ad65bcb3f0c96efe8f7cf80d507
denverjohnston <277109418+denverjohnston@users.noreply.github.com>
2026-06-23 20:52:47 -0700

    Renamed oomph to impact, set default mixer for sound event to "Game", and created a MinFallDistance property for the PlayerImpactSound component

--- Code/player/PlayerImpactSound.cs
+++ Code/player/PlayerImpactSound.cs
@@ -1,9 +1,10 @@
 using Sandbox;
 
-public sealed class PlayerOomphSound : Component, PlayerController.IEvents
+public sealed class PlayerImpactSound : Component, PlayerController.IEvents
 {
 	[Property] public PlayerController PlayerController { get; set; }
 	[Property] public SoundEvent Sound { get; set; }
+	[Property] public float MinFallDistance { get; set; } = 100f;
 
 	[Rpc.Broadcast]
 	private void BroadcastSound()
@@ -13,6 +14,6 @@ public sealed class PlayerOomphSound : Component, PlayerController.IEvents
 
 	void PlayerController.IEvents.OnLanded( float distance, Vector3 impactVelocity )
 	{
-		if ( distance > 100 ) BroadcastSound();
+		if ( distance > MinFallDistance ) BroadcastSound();
 	}
 }
